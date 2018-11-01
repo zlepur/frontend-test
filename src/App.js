e0ce7a7fba3e6b219dc1c2b8acfef11ddee9cf79
Zvonimir Lepur <zvonimir.lepur@gmail.com>
2018-11-01 13:31:11 +0100

    Add and use component RandomColor.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const BLACK = '#000000';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <RandomColor />
         </header>
+      </div>
+    );
+  }
+}
+
+class RandomColor extends Component {
+  constructor() {
+    super();
+    this.state = {
+      colors: [],
+      currentColor: BLACK,
+    };
+    this.onToggleColorClick = this.onToggleColorClick.bind(this);
+  }
+
+  async fetchColors() {
+    const init = {method: 'GET'};
+    let response = await fetch('http://www.colr.org/json/colors/random/2', init);
+    if(!response.ok) {
+      return [];
+    }
+    let data = await response.json();
+    return data.colors.map(color => '#' + color.hex);
+  }
+
+  componentDidMount() {
+    let colors = this.fetchColors();
+    colors.then(colors => this.setState({colors: colors}));
+  }
+
+  onToggleColorClick() {
+    let color;
+    if(this.state.currentColor === BLACK) {
+      color = this.state.colors[Math.floor(Math.random() * this.state.colors.length)];
+    } else {
+      color = BLACK;
+    }
+    this.setState({currentColor: color});
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="color-box" style={{backgroundColor: this.state.currentColor}}></div>
+        <button onClick={this.onToggleColorClick}>Toggle color</button>
       </div>
     );
   }
